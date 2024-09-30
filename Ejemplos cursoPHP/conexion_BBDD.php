e9f539a610398e1e4a70b088145892bf6816b554
Fran <fvsalapic@gmail.com>
2024-09-30 10:11:51 +0200

    formatted

--- Ejemplos cursoPHP/conexion_BBDD.php
+++ Ejemplos cursoPHP/conexion_BBDD.php
@@ -1,28 +1,30 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
-   <?php
-    $db_host="localhost";
-    $db_nombre="pruebas";
-    $db_usuario="root";
-    $db_contra="";
+    <?php
+    $db_host = "localhost";
+    $db_nombre = "pruebas";
+    $db_usuario = "root";
+    $db_contra = "";
 
     //REALIZAMOS LA CONEXION
-    $conexion = mysqli_connect($db_host,$db_usuario, $db_contra,$db_nombre);
+    $conexion = mysqli_connect($db_host, $db_usuario, $db_contra, $db_nombre);
 
     //CREAMOS LA CONSULTA
-    $consulta="SELECT * FROM datospersonales";
+    $consulta = "SELECT * FROM datospersonales";
 
     //GUARDAMOS EN UNA TABLA VIRTUAL EL RESULSET O RECORDSET
-    $resultados=mysqli_query($conexion,$consulta);
+    $resultados = mysqli_query($conexion, $consulta);
 
     //RECORREMOS FILA A FILA DICHA TABLA, DICHO RESULSET
-    $fila=mysqli_fetch_row($resultados);
+    $fila = mysqli_fetch_row($resultados);
 
     echo $fila[0] . " ";
     echo $fila[1] . " ";
@@ -31,14 +33,15 @@
 
     echo "<br>";
 
-    $fila=mysqli_fetch_row($resultados);
+    $fila = mysqli_fetch_row($resultados);
 
     echo $fila[0] . " ";
     echo $fila[1] . " ";
     echo $fila[2] . " ";
     echo $fila[3] . " ";
 
-   ?>
+    ?>
 
 </body>
+
 </html>
