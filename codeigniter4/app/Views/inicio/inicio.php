03f12bb9dd97636fa4b4ca96cdbcbd81f792cff9
Francisco <fvsalapic@outlook.com>
2025-01-13 09:39:31 +0100

    delete codeigniter

--- codeigniter4/app/Views/inicio/inicio.php
+++
@@ -1,4 +0,0 @@
-
-    <h1>Mi primera aplicación en CI4</h1>
-    <h3><?php echo $titulo ?></h3>
-    <div>Hoy es <?php echo $fecha; ?></div>
--- codeigniter4/app/Views/inicio/sobre_mi.php
+++
@@ -1,3 +0,0 @@
-
-    <h1>2ºDAW - VMNC</h1>
-    <p>Hola, soy VMNC y este es mi espacio de presentación.</p>
